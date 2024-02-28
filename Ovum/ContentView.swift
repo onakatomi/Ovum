3bc827fd4933a4c3b58067d283dcad7e60be40e1
Oliver Quarm <ollie@nakatomi.com>
2024-02-28 16:41:26 +1100

    Initial Commit

---
+++ Ovum/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Ovum
+//
+//  Created by Ollie Quarm on 28/2/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Ovum/OvumApp.swift
@@ -0,0 +1,17 @@
+//
+//  OvumApp.swift
+//  Ovum
+//
+//  Created by Ollie Quarm on 28/2/2024.
+//
+
+import SwiftUI
+
+@main
+struct OvumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
